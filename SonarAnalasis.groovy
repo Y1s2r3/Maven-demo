e7119523bf16b7744ae022f9cf75c0573f761898
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-19 10:05:45 +0530

    Update SonarAnalasis.groovy

--- SonarAnalasis.groovy
+++ SonarAnalasis.groovy
@@ -3,7 +3,7 @@
   git branch: 'main', url: 'https://github.com/Y1s2r3/Maven-demo.git'
   }
   stage('Build') {
-    bat "cd initial && mvn clean install"
+    bat "cd initial && mvn clean package"
   }
   stage ('analysis') {
    bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar \
